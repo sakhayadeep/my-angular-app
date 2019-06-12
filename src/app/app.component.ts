31b5ff253f6964675f1ae04c0c24dc9d8191be8b
Sakhayadeep <sakhayadeepnath@gmail.com>
2019-06-12 14:40:21 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormGroup, FormControl} from "@angular/forms"
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+
+  data = [
+    {
+      'name': "person 1",
+      'age': "32",
+      'email': "person1@gmail.com"
+    },
+    {
+      'name': "person 2",
+      'age': "33",
+      'email': "person2@gmail.com"
+    },
+    {
+      'name': "person 3",
+      'age': "34",
+      'email': "person3@gmail.com"
+    }
+  ];
+
+  userForm;
+  constructor(){
+    this.userForm = new FormGroup({
+      'name': new FormControl(),
+      'age': new FormControl(),
+      'email': new FormControl()
+    });
+  }
+
+  formSubmit(){
+    this.data.push(this.userForm.value);
+  }
+
 }
